43ef2d81aeb898e7c8d797fd1865da3d767eb3e3
Juan Diego Betancur <juandiegosmr142@gmail.com>
2023-03-27 11:58:14 +0200

    Configuración de Firebase añadida

--- app/src/main/java/com/juancho_dam/playtabs/MainActivity.java
+++ app/src/main/java/com/juancho_dam/playtabs/MainActivity.java
@@ -4,11 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.ktx.Firebase;
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database;
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
+
+
+
 }
