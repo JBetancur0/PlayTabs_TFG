daa1b4b1c036145defef96ee5ccd7d2d3a99491d
Juan Diego Betancur <juandiegosmr142@gmail.com>
2023-04-12 18:44:24 +0200

    Implementación de un menú lateral

--- app/src/main/java/com/juancho_dam/playtabs/main_menu_activity.java
+++ app/src/main/java/com/juancho_dam/playtabs/main_menu_activity.java
@@ -7,6 +7,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
@@ -27,14 +31,30 @@ public class main_menu_activity extends AppCompatActivity {
     private ListaTabsAdapter adaptadorTabs;
     private ArrayList<Tab> tabs;
     private DatabaseReference myRefTabs = null;
+    private ImageView img_menuBack;
+    private ImageView img_btnBack;
+    private Button btn_buscarTab;
+    private EditText input_nom_tab;
+    private ImageView img_user_pic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-        user_name = findViewById(R.id.txt_nombre_user);
         rv_tabsH = (RecyclerView) findViewById(R.id.rv_tabsH);
+        img_menuBack = findViewById(R.id.img_menuBack);
+        btn_buscarTab = findViewById(R.id.btn_buscar_tab);
+        img_btnBack = findViewById(R.id.img_btnBack);
+        input_nom_tab = findViewById(R.id.input_nom_tab);
+        user_name = findViewById(R.id.txt_user_name);
+        img_user_pic = findViewById(R.id.img_user_pic);
+
+        img_menuBack.setAlpha(0.0f);
+        img_btnBack.setAlpha(0.0f);
+        img_btnBack.setClickable(false);
+        user_name.setAlpha(0.0f);
+        img_user_pic.setAlpha(0.0f);
 
         LinearLayoutManager manager = new LinearLayoutManager(this);
         rv_tabsH.setLayoutManager(manager);
@@ -76,8 +96,43 @@ public class main_menu_activity extends AppCompatActivity {
         FirebaseUser user = i.getParcelableExtra(iniciar_sesion_activity.EXTRA_CURRENT_USER);
 
         String user_nameFirebase = user.getDisplayName();
-
         user_name.setText(user_nameFirebase);
+
+
+
+    }
+
+    public void open_menu(View view) {
+
+        img_btnBack.setClickable(true);
+        input_nom_tab.setClickable(false);
+        input_nom_tab.setFocusable(false);
+        rv_tabsH.setClickable(false);
+        rv_tabsH.setFocusable(false);
+        input_nom_tab.setFocusableInTouchMode(false);
+        img_menuBack.animate().alpha(1.0f).setDuration(300);
+        btn_buscarTab.animate().alpha(0.0f).setDuration(300);
+        img_btnBack.animate().alpha(1.0f).setDuration(300);
+        user_name.animate().alpha(1.0f).setDuration(300);
+        img_user_pic.animate().alpha(1.0f).setDuration(300);
+
+
+
+    }
+
+    public void hide_menu(View view) {
+
+        img_btnBack.setClickable(false);
+        input_nom_tab.setClickable(true);
+        input_nom_tab.setFocusable(true);
+        rv_tabsH.setClickable(true);
+        rv_tabsH.setFocusable(true);
+        input_nom_tab.setFocusableInTouchMode(true);
+        img_menuBack.animate().alpha(0.0f).setDuration(300);
+        img_btnBack.animate().alpha(0.0f).setDuration(300);
+        btn_buscarTab.animate().alpha(1.0f).setDuration(300);
+        user_name.animate().alpha(0.0f).setDuration(300);
+        img_user_pic.animate().alpha(0.0f).setDuration(300);
 
     }
 }
